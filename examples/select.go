e28fc77a6f659defc2d9a274e57a5075caa18fd6
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-08-30 19:53:36 +0530

    Examples: select.

---
+++ examples/select.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// RandStringRunes generates random strings of length n
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func randomString(messages chan string) {
+	messages <- RandStringRunes(rand.Intn(10) + 1)
+}
+
+func randomInt(numbers chan int) {
+	numbers <- rand.Intn(100)
+}
+
+func main() {
+	messages := make(chan string)
+	numbers := make(chan int)
+
+	for i := 1; i <= 5; i++ {
+		go randomString(messages)
+		go randomInt(numbers)
+	}
+
+OUTER:
+	for {
+		select {
+		case msg := <-messages:
+			fmt.Println("Message: ", msg)
+		case num := <-numbers:
+			fmt.Println("Number: ", num)
+		default:
+			break OUTER
+		}
+	}
+}
